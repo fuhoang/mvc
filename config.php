008626c136b68dfc6e8108097ba21ee0ed2c3acd
fuhoang <fuhoang84@googlemail.com>
2013-09-13 21:46:09 +0100

    config.php for mvc framework

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+define('DB_TYPE', 'mysql');
+define('DB_HOST','localhost');
+define('DB_NAME', 'new_era_geeks');
+define('DB_USER', 'root');
+define('DB_PASS', 'chinaman');
+
+//Salt for password setting
+define('HASH_GENERAL_KEY', 'Ch1n4m4n');
+//Database password only
+define('HASH_PASSWORD_KEY', 'Ch1n4m4n1984');
+
+//define paths
+define('URL', 'http://localhost/mvc/');
+define('LIBS', 'libs/');
+?>
